77dff75ec21dab02e605f48387b0287a0e4dca9b
Samoilenko Yuri <kinnalru@gmail.com>
2017-02-22 14:16:49 -0400

    fix

--- bluebutton.rb
+++ bluebutton.rb
@@ -1,14 +1,35 @@
 #!/usr/bin/ruby
 
-DEVNAME="AB Shutter3"
+require 'device_input'
+
+BUTTON_NAME="AB Shutter3"
 
 def find_device
   devices = `xinput`.split("\n").select{|d| d["AB"]}
 end
 
-puts `xinput`.length
-puts `xinput`.split("\n").select{|p| p['AB']}
-puts "---"
+def find_device_sys
+  finded = Dir.glob("/sys/**/name").select do |file|
+    File.read(file)[BUTTON_NAME]
+  end
+
+  "/dev/" + finded.map do |file|
+    Dir.glob("#{File.dirname(file)}/**/uevent").map do |file|
+      File.read(file).split("\n").select{|s| s['DEVNAME']}.compact.first
+    end.compact
+  end.flatten.first.split("=")[1]
+
+rescue nil
+end
+
+File.open(find_device_sys, 'rb' ) do |input|
+  DeviceInput.read_loop(input) do |event|
+    if event.type == 'EV_KEY'
+      puts event
+    end
+  end
+end
+
+puts find_device_sys
 
 
-find_device
